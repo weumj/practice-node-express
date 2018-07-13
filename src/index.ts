a76134af3153382e5eda3bf0b5eb496df38363d3
Myungjin Kim <weumj1@gmail.com>
2018-07-13 14:45:55 +0900

    env: setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-// import express from 'express';
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world!');
+});
+
+app.listen('3000', () => {
+  console.log('listening on port 3000');
+});
